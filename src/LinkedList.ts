b1d9af1500e2c0ee51b0ddc64202d921a6f7dfa3
songqingzhao <isongqingzhao@163.com>
2021-12-22 13:51:42 +0800

    完善链表方法

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -9,6 +9,9 @@ class LinkedNode {
     }
 }
 
+type baseType = number | string;
+type referenceType = {[str: string]: unknown} | any[] | Map<any, any> | Set<unknown> | Function;
+
 class LinkedList {
     count: number;
     head: LinkedNode | undefined;
@@ -18,8 +21,64 @@ class LinkedList {
         this.head = undefined;
     }
 
-    equals(itemA: unknown, itemB: unknown) {
+    getHead() {
+        return this.head;
+    }
+
+    equals(itemA: baseType, itemB: baseType) {
         return itemA === itemB;
+    }
+
+    // TODO: 比较有问题，以后再说懒得修改
+    equalsObj(itemA: referenceType, itemB: referenceType) {
+        let itemAStr = [],
+            itemBStr = [];
+        switch(Object.prototype.toString.call(itemA)) {
+            case '[object Set]':
+            case '[object Map]':
+            case '[object Array]':
+                (itemA as [] | Map<any, any> | Set<unknown>).forEach((val, key) => {
+                    itemAStr.push(`${key}=${val}`)
+                });
+                (itemB as [] | Map<any, any> | Set<unknown>).forEach((val, key) => {
+                    itemBStr.push(`${key}=${val}`)
+                })
+                break;
+            case '[object Object]':
+                Object.entries(itemA).forEach((val, key) => {
+                    itemAStr.push(`${key}=${val}`)
+                })
+                Object.entries(itemB).forEach((val, key) => {
+                    itemBStr.push(`${key}=${val}`)
+                })
+                break;
+            case '[object Function]':
+                itemAStr.push(itemA.toString())
+                itemBStr.push(itemB.toString())
+                break;
+        }
+        return itemAStr.join(',') === itemBStr.join(',');
+    }
+
+    /**
+     * 获取元素在链表中的位置
+     * @param {*} element 元素
+     * */
+    indexOf(element: unknown) {
+        let currentNode = this.head,
+            index = 0,
+            equalsFn;
+        if (['number', 'string'].includes(typeof element)) {
+            equalsFn = this.equals;
+        } else {
+            equalsFn = this.equalsObj;
+        }
+
+        while(currentNode && currentNode.next && !equalsFn(element as any, currentNode.element as any)) {
+            currentNode = currentNode.next;
+            index++;
+        }
+        return index;
     }
 
     push(element: unknown) {
@@ -37,26 +96,55 @@ class LinkedList {
         prevLinkNode.next = current;
     }
 
+    /**
+     * 在任意位置插入元素
+     * @param {*} element 插入的元素
+     * @param {Number} index 插入的位置
+     * */
     insert(element: unknown, index: number) {
-
+        const previous = this.getElementAt(index - 1);
+        if (previous) {
+            const currentNode = new LinkedNode(element);
+            currentNode.next = previous.next;
+            previous.next = currentNode;
+            this.count++;
+            return true;
+        }
+        this.push(element);
+        return true;
     }
 
-    getElementAt(element: unknown): number {
-        return -1;
+    getElementAt(index: number): LinkedNode | undefined {
+        let loopCount = 0,
+            currentNode = this.head;
+        while(currentNode && currentNode.next && loopCount > index) {
+            currentNode = currentNode.next;
+            loopCount++;
+        }
+        return currentNode;
     }
+
+    /**
+     * 根据下标删除链表元素
+     * @param {Number} index 要删除的元素索引
+     * */
+    removeAt(index: number): unknown {
+        let currentNode = this.head;
+        if (index === 0 && currentNode) {
+            this.head = currentNode.next;
+        }
+        let previous = this.getElementAt(index - 1);
+        if (previous) {
+            currentNode =  previous.next;
+            previous.next = currentNode?.next;
+        }
+        this.count--;
+        return currentNode?.element
+    }
+
 
     remove(element: unknown) {
-        let currentNode = this.head,
-            isRemove = false;
-        while(currentNode?.next && currentNode.element != element) {
-            currentNode = currentNode.next;
-            if (currentNode.next?.element === element) {
-                currentNode.next = currentNode.next?.next;
-                this.count--;
-                isRemove = true;
-            }
-        }
-        return isRemove ? element : undefined;
+        return this.removeAt(this.indexOf(element));
     }
 
     isEmpty() {
@@ -84,5 +172,6 @@ class LinkedList {
 
 
 export {
-    LinkedList
+    LinkedList,
+    LinkedNode
 }
